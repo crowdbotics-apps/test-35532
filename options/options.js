9834d1a768e8ad9ac8b3b06676f51c3b79c339c1
Crowdbotics <team@crowdbotics.com>
2022-05-31 18:38:14 +0000

    Update options

--- options/options.js
+++ options/options.js
@@ -10,6 +10,7 @@ export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   },
+
   "@modules/login": {
     LOGO_URL:
       "https://crowdbotics-slack-dev.s3.amazonaws.com/media/project_component_resources/cb-icon.png",
@@ -19,7 +20,7 @@ export const modulesOptions = {
     validateEmail: "^[^\\s]+([.-]?\\w+)*@\\w+([.-]?\\w+)*(.\\w{2,3})+$",
     SignInNavText: "Sign In",
     SignUpNavText: "Sign Up",
-    SignInButtonText: "Login",
+    SignInButtonText: "Logi",
     SignUpButtonText: "Sign Up"
   }
 }
